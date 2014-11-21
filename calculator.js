bb221fc2192231ece617a2ce12b9f42aa8c0dbd8
Joe McDonough <2jomcd@gmail.com>
2014-11-21 10:19:20 -0800

    joe night 1 work

---
+++ calculator.js
@@ -0,0 +1,7 @@
+//document.querySelector("#light1").addEventListener("mouseover", function() {
+//	document.querySelector("#light1").setAttribute("fill", "red");
+//});
+
+//document("calculator")
+document.querySelector("#buttons_row_1").addEventListener("mouseover", function() {
+	document.querySelector("#buttons_row_1").setAttribute("border-color", "red")});
